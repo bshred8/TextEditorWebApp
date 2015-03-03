b161b5fb3dbeeb22e671db2c803e540a190e8d86
Brady <bshred8@kronosad.com>
2015-03-03 00:57:17 -0500

    Second commit

--- Javascript/settings.js
+++ Javascript/settings.js
@@ -1,19 +1,3 @@
-var download = function() {
-	var allTextInputed = $("#main-input").val();
-
-	$("html").html(allTextInputed);
-	alert(
-		"Press [Enter] or click [OK] and then press [Control] + [S] to download."+
-	    " Remember to change the .html to .txt!");
-}
-
-
-
-
 $(document).ready(function() {
-
-	$("#download-button").click(function() {
-		download();
-	});
-
+	
 });
